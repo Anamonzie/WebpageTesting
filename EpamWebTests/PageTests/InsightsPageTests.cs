8d8dab905f5feb2c151879efb8fcb63413ffc5c9
Anamonzie <169081947+Anamonzie@users.noreply.github.com>
2024-10-29 14:59:30 +0400

    Update InsightsPageTests.cs

--- EpamWebTests/PageTests/InsightsPageTests.cs
+++ EpamWebTests/PageTests/InsightsPageTests.cs
@@ -115,7 +115,7 @@ namespace EpamWebTests.PageTests
                 // Capture a screenshot before closing
                 var screenshotsDirectory = Path.Combine("Screenshots", TestContext.CurrentContext.Test.Name);
                 Directory.CreateDirectory(screenshotsDirectory);                
-                var screenshotPath = Path.Combine(screenshotsDirectory, $"screenshot_{DateTime.UtcNow:yyyyMMdd_HHmmss}.png");
+                var screenshotPath = Path.Combine(screenshotsDirectory, $"screenshot_{DateTime.UtcNow:MMdd_HHmm}.png");
                 Log.Information($"Captured screenshot at {screenshotPath}. (Insigts Page Tests)");
 
                 await page.ScreenshotAsync(new()
