52b3f0ab4ab7e5428e973ec628b4fc54d19cf6b7
Anamonzie <169081947+Anamonzie@users.noreply.github.com>
2024-11-14 16:08:54 +0400

    added attachments in allureattachmentmanager

--- EpamWebProject/EpamWeb/Attachments/AllureAttachmentManager.cs
+++ EpamWebProject/EpamWeb/Attachments/AllureAttachmentManager.cs
@@ -6,8 +6,6 @@ namespace EpamWeb.Attachments
 {
     public class AllureAttachmentManager : IAllureAttachmentManager
     {
-        //private static readonly object logLock = new();
-
         public async Task AddScreenshotAttachment(string screenshotPath)
         {
             await Task.Run(() => AllureApi.AddAttachment("Screenshot", "image/png", screenshotPath));
@@ -22,21 +20,14 @@ namespace EpamWeb.Attachments
                 var videoPath = Path.Combine("videos", path);
 
                 AllureApi.AddAttachment("Test Video", "video/webm", videoPath);
+                TestContext.AddTestAttachment(videoPath);
             }
         }
 
         public void AttachLogToAllure(string logFilePath)
         {
             AllureApi.AddAttachment("Test Logs", "text/plain", logFilePath);
-
-            //lock (logLock) // Ensure only one thread can access this block at a time
-            //{
-            //    if (logFilePath != null && logFilePath.Exists)
-            //    {
-            //        var logsPath = logFilePath.FullName;
-            //        AllureApi.AddAttachment("Test Logs", "text/plain", logsPath);
-            //    }
-            //}
+            TestContext.AddTestAttachment(logFilePath);
         }
     }
 }
